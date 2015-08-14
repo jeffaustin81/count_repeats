a5f3e39e9697a0daa956fbe9e624aea1403d8c70
Jeff Austin <jeffaustin81@gmail.com>
2015-08-14 11:16:12 -0700

    Passed test 3 and added test 4

--- tests/CountRepeatsTest.php
+++ tests/CountRepeatsTest.php
@@ -3,6 +3,11 @@
 	
 	class CountRepeatsTest extends PHPUnit_Framework_TestCase
 	{
+		// I had to comment out this test because
+		// it expects a boolean return and I couldn't
+		// get it to pass as all tests moving
+		// forward would expect an integer return
+		// not sure how I could of avoided it?
 		// function test_countWord_wordtrue()
 		// {
 		// 	$test_CountRepeats = new CountRepeats;
@@ -23,6 +28,28 @@
 			$result = $test_CountRepeats->countWord($input1, $input2);
 			
 			$this->assertEquals(2, $result);
+		}
+		
+		function test_countWord_wordcount2()
+		{
+			$test_CountRepeats = new CountRepeats;
+			$input1 = "How much wood would a woodchuck chuck if a woodchuck could chuck wood";
+			$input2 = "wood";
+			
+			$result = $test_CountRepeats->countWord($input1, $input2);
+			
+			$this->assertEquals(2, $result);
+		}
+		
+		function test_countWord_wordcount2()
+		{
+			$test_CountRepeats = new CountRepeats;
+			$input1 = "Where is Portland? How many times have you been to Portland? What is Portland like?";
+			$input2 = "Portland";
+			
+			$result = $test_CountRepeats->countWord($input1, $input2);
+			
+			$this->assertEquals(3, $result);
 		}
 	}
 ?>
