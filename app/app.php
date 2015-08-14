627337045c249b709c3193296f539520dbf6684d
Jeff Austin <jeffaustin81@gmail.com>
2015-08-14 14:45:49 -0700

    Added results route and added results.html.twig view, the view will display the results of the search

--- app/app.php
+++ app/app.php
@@ -2,11 +2,16 @@
 	// This will be our dependencies for this
 	// file
 	require_once __DIR__."/../vendor/autoload.php";
-	require_once __DIR__."/../src/CountRepeats.php";
+	require_once __DIR__."/../src/RepeatCounter.php";
+	
+	use Symfony\Component\Debug\Debug;
+	Debug::enable();
 	
 	// This sets up a new instance of our
 	// Silex Application
 	$app = new Silex\Application();
+	
+	$app['debug'] = true;
 	
 	// This registers Twig and lets Twig know
 	// where our views will be stored
@@ -17,7 +22,15 @@
 	// This sets up the home page route and 
 	// renders our home page view
 	$app->get("/", function() use ($app) {
-		return $app['twig']->render('home_page.html.twig');
+		return $app['twig']->render('home.html.twig');
+	});
+	
+	$app->get("/results", function() use ($app) {
+		$wordcount = new RepeatCounter;
+		$get_wordcount = $wordcount->countWord($_GET['sentence'], $_GET['word']);
+		
+		
+		return $app['twig']->render('results.html.twig', array('results' => $get_wordcount));
 	});
 	
 	return $app;
---
+++ src/RepeatCounter.php
@@ -0,0 +1,59 @@
+<?php 
+class RepeatCounter
+{
+	function countWord($input1, $input2) 
+	{
+		// This sets both inputs to lowercase
+		// to avoid cap issues
+		$input1_low = strtolower($input1);
+		$input2_low = strtolower($input2);
+		
+		// Renamed input2 for clarity and converted 
+		// to an associative array and assigned
+		// an integer value of one to keep count
+		// of instances of the word
+		$word = $input2_low;
+		// Array below commented out because 
+		// foreach further down wouldn't run
+		// even though it passed in test
+		// $word_count = array($word => 1);
+		
+		// This will explode input1 into an array
+		// of words then replace anything that is
+		// not a letter with nothingness.
+		$sentence = explode(" ", $input1_low);
+		$sentence = preg_replace("/[^A-Za-z]/", "", $sentence);
+		
+		// This is the counter variable that 
+		// will keep track of instances of the 
+		// word searched
+		$count = 0;
+		
+		// This will run through the sentence array
+		// and add 1 to the counter each time
+		// the word in the sentence matches
+		// the word in the associative array
+		foreach($sentence as $index) {
+			// Below commented out because it 
+			// failed to run in Silex even though
+			// it passed tests. I was getting a
+			// undefined error for the first index
+			// of $sentence...
+			
+			// $count += $word_count[$index];
+			
+			// Added an if check instead
+			if($index === $word) {
+				$count++;
+			}
+		}
+		
+		// After the foreach loop is run we
+		// return the count
+		return $count;
+		
+		
+		
+	}
+}
+?>
