74ef560bf9eb2ddfc0832712b088dd409b2f9f18
Jeff Austin <jeffaustin81@gmail.com>
2015-08-14 12:01:57 -0700

    Added comments to index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+	//  This is where all the magic happens
+	// this will be what the browser interacts
+	// with, accessing our app file and running
+	//  our routes
 	$website = require_once __DIR__.'/../app/app.php';
 	$website->run();
 ?>
