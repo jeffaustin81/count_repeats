ffb98310393de9545f526d6b517a5b6ee3c12b07
Jeff Austin <jeffaustin81@gmail.com>
2015-08-14 11:59:15 -0700

    Added comments to app.php

--- app/app.php
+++ app/app.php
@@ -1,13 +1,21 @@
-<?php 
+<?php
+	// This will be our dependencies for this
+	// file
 	require_once __DIR__."/../vendor/autoload.php";
 	require_once __DIR__."/../src/CountRepeats.php";
 	
+	// This sets up a new instance of our
+	// Silex Application
 	$app = new Silex\Application();
 	
+	// This registers Twig and lets Twig know
+	// where our views will be stored
 	$app->register(new Silex\Provider\TwigServiceProvider(), array(
 		'twig.path' => __DIR__.'/../views'
 	));
 	
+	// This sets up the home page route and 
+	// renders our home page view
 	$app->get("/", function() use ($app) {
 		return $app['twig']->render('home_page.html.twig');
 	});
